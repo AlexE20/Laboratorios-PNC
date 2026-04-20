b3fa35ff10e6c557c683bcc7cb3c2b00dae688f7
AlexE20 <00009423@uca.edu.sv>
2026-04-20 12:47:32 -0600

    :construction:Working on Pokemon Database creation

---
+++ Laboratorio1/src/main/java/com/example/laboratorio1/common/PokemonData.java
@@ -0,0 +1,40 @@
+package com.example.laboratorio1.common;
+
+import com.example.laboratorio1.domain.entities.Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokemonData {
+    private final List<Pokemon> listaPokemon = new ArrayList<>();
+
+    public PokemonData(){
+        listaPokemon.add(Pokemon.builder()
+                .nombre("Charmander")
+                .tipo("Fuego")
+                .debilidades(List.of("Agua", "Roca", "Tierra"))
+                .zonas(List.of("Montaña", "Cueva"))
+                .region("Kanto")
+                .build());
+
+        listaPokemon.add(Pokemon.builder()
+                .nombre("Rayquasa")
+                .tipo("Volador")
+                .debilidades(List.of("Electrico", "Hielo", "Roca"))
+                .zonas(List.of("Montaña", "Cueva"))
+                .region("Kanto")
+                .build());
+
+        listaPokemon.add(Pokemon.builder()
+                .nombre("Pikachu")
+                .tipo("Eléctrico")
+                .debilidades(List.of("Tierra"))
+                .zonas(List.of("Bosque", "Ciudad"))
+                .region("Kanto")
+                .build());
+    }
+
+    public List<Pokemon> getLista() {
+        return listaPokemon;
+    }
+}
