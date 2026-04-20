c32433f1c5bbe677b6ac87789781750f258d2fad
AlexE20 <00009423@uca.edu.sv>
2026-04-20 12:36:34 -0600

    :construction:Working on Pokemon class creation

---
+++ Laboratorio1/src/main/java/com/example/laboratorio1/domain/entities/Pokemon.java
@@ -0,0 +1,21 @@
+package com.example.laboratorio1.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Pokemon {
+    private String nombre;
+    private String tipo;
+    private List<String> debilidades;
+    private List<String> zonas;
+    private String region;
+}
