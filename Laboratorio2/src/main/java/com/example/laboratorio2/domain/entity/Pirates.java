2eb1d0747e4900c330539f87b86b76964169f678
AlexE20 <00009423@uca.edu.sv>
2026-05-04 21:36:35 -0600

    feat:Starting Laboratorio2 And adding Pirate entity

---
+++ Laboratorio2/src/main/java/com/example/laboratorio2/domain/entity/Pirates.java
@@ -0,0 +1,25 @@
+package com.example.laboratorio2.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="Pirata")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pirates {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private Double bounty;
+    private String crew;
+    private Boolean isAlive;
+}
