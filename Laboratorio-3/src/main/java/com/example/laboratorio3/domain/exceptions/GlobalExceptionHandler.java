93582e5e38d4d254b6733abe954adc96b369c7ae
AlexE20 <00009423@uca.edu.sv>
2026-05-18 22:58:21 -0600

    fix:fixing bug about validations

--- Laboratorio-3/src/main/java/com/example/laboratorio3/domain/exceptions/GlobalExceptionHandler.java
+++ Laboratorio-3/src/main/java/com/example/laboratorio3/domain/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.example.laboratorio3.domain.dto.response.ApiErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,6 +28,35 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationException(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
+    ) {
+
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error ->
+                        error.getDefaultMessage() != null
+                                ? error.getDefaultMessage()
+                                : "Validation error"
+                )
+                .findFirst()
+                .orElse("Validation error");
+
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMessage)
+                .timestamp(LocalDateTime.now())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(error);
     }
 }
